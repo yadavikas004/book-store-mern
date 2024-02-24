789b43c4ef45101ad103bbe162b0153c80cfa20a
Vikas Yadav <vikas.yadav@softwareworkshop.net>
2024-02-24 14:02:38 +0530

    (development): added cors and node files for frontend

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,18 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1: Allow all Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow custom origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/',(request,response)=>{
     console.log(request);
     return response.status(234).send("Welcome To Mern Stack Tutorial");
