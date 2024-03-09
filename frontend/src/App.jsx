f8378bad577fd5224063822841674c9c91e60b14
Vikas Yadav <yadavikas088@gmail.com>
2024-03-10 01:03:06 +0530

    app.jsx and main.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBooks from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
@@ -8,13 +8,16 @@ import EditBook from './pages/EditBook';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/books/create' element={<CreateBooks/>}/>
-      <Route path='/books/details/:id' element={<ShowBook/>}/>
-      <Route path='/books/edit/:id' element={<EditBook/>}/>
-      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBooks />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
+    </Router>
+
   );
 };
 
